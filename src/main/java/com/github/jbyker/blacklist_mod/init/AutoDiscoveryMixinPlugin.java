0b4ad4dcfa54fad87044cad92cc75cbbc92f680e
GitHub Action <action@github.com>
2025-12-23 17:36:21 +0000

    Initialize template repository proper
    Set package name to com.github.jbyker.blacklist_mod
    Set project name to Blacklist_Mod
    Set modid to blacklistmod

--- src/main/java/com/github/jbyker/blacklist_mod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/jbyker/blacklist_mod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.jbyker.blacklist_mod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
